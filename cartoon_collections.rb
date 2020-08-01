680c2b3b15903ec5ffffbce9a84b7f21e44a6968
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-01 22:01:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ def find_the_cheese(cheeses) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if cheeses.include?(cheese)
-      puts cheese
+      return cheese
     end
   end
 end
